20cd7524d3908e84c11d37e9ea1e47d4e3655558
Joshua Olson <joshua.olson@gmail.com>
2011-12-03 10:09:42 -0800

    Adjusted rollover function to work with image galleries too

--- wyldthingz.js
+++ wyldthingz.js
@@ -14,22 +14,34 @@ function cache_images_rollovers() {
     for(var i = 0; i < document.images.length; i++) {
 	var img = document.images[i];
 	var rollover = img.getAttribute("data-rollover");
+	var thumbnail = img.getAttribute("thumbnail");
 	
-	// Don't need to preload non-rollover images
-	if (!rollover) {
-	    (new Image()).src = img.src;
-	    continue;
+	// For image changes on hover/rollover
+	if(rollover) {
+	    // Ensure that the rollover image is in the cache
+	    (new Image()).src = rollover;
+
+	    img.setAttribute("data-rollout", img.src);
+	    img.onmouseover = function() {
+		this.src = this.getAttribute("data-rollover");
+	    };
+	    img.onmouseout = function() {
+		this.src = this.getAttribute("data-rollout");
+	    };
 	}
 
-	// Ensure that the rollover image is in the cache
-	(new Image()).src = rollover;
-
-	img.setAttribute("data-rollout", img.src);
-	img.onmouseover = function() {
-	    this.src = this.getAttribute("data-rollover");
-	};
-	img.onmouseout = function() {
-	    this.src = this.getAttribute("data-rollout");
-	};
+	// Update an image based on the thumbnail you rollover/click
+	if(img.getAttribute("thumbnail")) {
+	    img.onmouseover = function() {
+		var thumb = this.getAttribute("thumbnail");
+		var frame = document.getElementById(thumb);
+		frame.src = this.src;
+	    };
+	    img.onclick = function() {
+		var thumb = this.getAttribute("thumbnail");
+		var frame = document.getElementById(thumb);
+		frame.src = this.src;
+	    };
+	}	    
     }
 }
